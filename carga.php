70cab9ad0ab274583eaf82a2baca6cebf325047b
Nicolas <nicolascardo70@gmail.com>
2022-09-16 12:09:06 -0300

    carga y modificacion de vehiculos

---
+++ carga.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Carga</title>
+</head>
+<body>
+    <center>
+        <form>
+            <label> Marca </label><br>
+            <input type="text" name= "marca" placeholder="Ej:  Fiat" id="marca"><br>
+            <label> Modelo </label><br>
+            <input type="text" name= "modelo" placeholder="Ej:  Camaro" id="modelo"><br>
+            <label> Color </label><br>
+            <input type="text" name= "color" placeholder="Ej:  Negro" id="color"><br>
+            <label> Año </label><br>
+            <input type="text" name= "anio" placeholder="Ej:  2017" id="anio"><br>
+            <label> Kilometros </label><br>
+            <input type="text" name= "kilo" placeholder="Ej:  0" id="kilo"><br>
+            <label> Precio </label><br>
+            <input type="text" name= "precio" placeholder="Ej:  2000000" id="precio"><br>
+            <label> Cuotas </label><br>
+            <input type="text" name= "cuota" placeholder="Ej:  12" id="cuota"><br>
+            <label> Patente </label><br>
+            <input type="text" name= "patente" placeholder="Ej:  HHY 584" id="patente"><br>
+            <label> Estado </label><br>
+            <input type="text" name= "estado" placeholder="Ej:  Disponible" id="estado"><br><br>
+            <button>Cargar Auto</button>
+        </form>
+    </center>
+</body>
+</body>
+</html>
---
+++ modificacion.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Modificacion</title>
+</head>
+<body>
+    <center>
+        <form>
+            <label>Colocar Patente del vehiculo a modificar</label><br>
+            <input type="text" name="matriculas" placeholder="Ej:  HHY 584" id="matriculas"><br><br>
+            <button>Cargar Datos</button><br><br>
+            <label> Marca </label><br>
+            <input type="text" name="marca" placeholder="Ej:  Fiat" id="marca"><br>
+            <label> Modelo </label><br>
+            <input type="text" name="modelo" placeholder="Ej:  Camaro" id="modelo"><br>
+            <label> Color </label><br>
+            <input type="text" name="color" placeholder="Ej:  Negro" id="color"><br>
+            <label> Año </label><br>
+            <input type="text" name="anio" placeholder="Ej:  2017" id="anio"><br>
+            <label> Kilometros </label><br>
+            <input type="text" name="kilo" placeholder="Ej:  0" id="kilo"><br>
+            <label> Precio </label><br>
+            <input type="text" name="precio" placeholder="Ej:  2000000" id="precio"><br>
+            <label> Cuotas </label><br>
+            <input type="text" name="cuota" placeholder="Ej:  12" id="cuota"><br>
+            <label> Patente </label><br>
+            <input type="text" name="patente" placeholder="Ej:  HHY 584" id="patente"><br>
+            <label> Estado </label><br>
+            <input type="text" name="estado" placeholder="Ej:  Disponible" id="estado"><br><br>
+            <button>Guardar Modificacion</button>
+        </form>
+    </center>
+</body>
+</html>
