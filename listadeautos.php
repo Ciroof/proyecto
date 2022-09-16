2207773fc60625c9f51cb3f2529b50fb0b98e15b
ciro figueroa <figueroaciro618@gmail.com>
2022-09-16 12:08:24 -0300

    listado de autos php

---
+++ listadeautos.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>
+        AUTOS
+    </title>
+</head>
+<body>
+<header>
+    <nav>
+        <a href=""> MODIFICACIONES</a>
+        <a href="">CREAR AUTO</a>
+    </nav>
+</header>
+<center>
+<div>
+    <h3>LISTADO DE AUTOS</h3>
+    <table style="border: 1px solid black; ">
+        <thead>
+            <tr>
+                <th style="border: 1px solid black;"colspan="4">marca:</th>
+                <th style="border: 1px solid black;"colspan = "4">modelo :</th>
+                <th style="border: 1px solid black;"colspan = "4">año: </th>
+                <th style="border: 1px solid black;"colspan = "4">color :</th>
+                <th style="border: 1px solid black;"colspan = "4">kilometros :</th>
+                <th style="border: 1px solid black;"colspan = "4"> preciofinal:</th>
+                <th style="border: 1px solid black;"colspan = "4"> cuotas: </th>
+                <th style="border: 1px solid black;"colspan = "4"> patente: </th>
+                <th style="border: 1px solid black;"colspan = "4"> estados: </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td style="border: 1px solid black;"colspan="4">marca:</td>
+                <td style="border: 1px solid black;"colspan = "4">modelo :</td>
+                <td style="border: 1px solid black;"colspan = "4">año: </td>
+                <td style="border: 1px solid black;"colspan = "4">color :</td>
+                <td style="border: 1px solid black;"colspan = "4">kilometros :</td>
+                <td style="border: 1px solid black;"colspan = "4"> preciofinal:</td>
+                <td style="border: 1px solid black;"colspan = "4"> cuotas: </td>
+                <td style="border: 1px solid black;"colspan = "4"> patente: </td>
+                <td style="border: 1px solid black;"colspan = "4"> estados: </td>
+            </tr>
+            <tr>
+                <td style="border: 1px solid black;"colspan="4">marca:</td>
+                <td style="border: 1px solid black;"colspan = "4">modelo :</td>
+                <td style="border: 1px solid black;"colspan = "4">año: </td>
+                <td style="border: 1px solid black;"colspan = "4">color :</td>
+                <td style="border: 1px solid black;"colspan = "4">kilometros :</td>
+                <td style="border: 1px solid black;"colspan = "4"> preciofinal:</td>
+                <td style="border: 1px solid black;"colspan = "4"> cuotas: </td>
+                <td style="border: 1px solid black;"colspan = "4"> patente: </td>
+                <td style="border: 1px solid black;"colspan = "4"> estados: </td>
+            </tr>
+            <tr>
+                <td style="border: 1px solid black;"colspan="4">marca </td>
+                <td style="border: 1px solid black;"colspan = "4">modelo</td>
+                <td style="border: 1px solid black;"colspan = "4">año: </td>
+                <td style="border: 1px solid black;"colspan = "4">color :</td>
+                <td style="border: 1px solid black;"colspan = "4">kilometros :</td>
+                <td style="border: 1px solid black;"colspan = "4"> preciofinal:</td>
+                <td style="border: 1px solid black;"colspan = "4"> cuotas: </td>
+                <td style="border: 1px solid black;"colspan = "4"> patente: </td>
+                <td style="border: 1px solid black;"colspan = "4"> estados: </td>
+            </tr>
+            <tr>
+                <td style="border: 1px solid black;"colspan="4">marca: </td>
+                <td style="border: 1px solid black;"colspan = "4">modelo :</td>
+                <td style="border: 1px solid black;"colspan = "4">año: </td>
+                <td style="border: 1px solid black;"colspan = "4">color :</td>
+                <td style="border: 1px solid black;"colspan = "4">kilometros :</td>
+                <td style="border: 1px solid black;"colspan = "4"> preciofinal:</td>
+                <td style="border: 1px solid black;"colspan = "4"> cuotas: </td>
+                <td style="border: 1px solid black;"colspan = "4"> patente: </td>
+                <td style="border: 1px solid black;"colspan = "4"> estados: </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+</center>
+
+</body>
+</html>
